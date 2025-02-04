dcc5be58d612dab9cdf8cfd0a48ea7e18ec4b6e9
Eric Pearson <108878682+MrEricPearson@users.noreply.github.com>
2025-02-04 11:31:03 -0600

    Update cs-calendar-mgmt.js

--- cs-calendar-mgmt.js
+++ cs-calendar-mgmt.js
@@ -2,7 +2,7 @@
 // @name         Crime Syndicate Calendar Management Tool
 // @namespace    https://github.com/MrEricPearson
 // @version      0.2.22
-// @description  Adds calendar management capabilities for your faction.
+// @description  Adds calendar management capabilities to your faction.
 // @author       BeefDaddy
 // @downloadURL  https://github.com/MrEricPearson/Crime-Syndicate-Calendar-Management-Tool/raw/refs/heads/main/cs-calendar-mgmt.js
 // @updateURL    https://github.com/MrEricPearson/Crime-Syndicate-Calendar-Management-Tool/raw/refs/heads/main/cs-calendar-mgmt.js
